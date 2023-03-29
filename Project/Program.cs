84a713a3eb0adb297b28cbcd37a1dcd9a46ade3a
Révész Ferenc <ferrier.revesz@gmail.com>
2023-03-29 21:03:47 +0200

    Added automigration on startup

--- Project/Program.cs
+++ Project/Program.cs
@@ -99,4 +99,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<CourseStatisticsContext>();
+    db.Database.Migrate();
+}
+
+
 app.Run();
