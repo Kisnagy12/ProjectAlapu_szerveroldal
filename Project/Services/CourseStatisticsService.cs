6f5dfbee0c2547c4246197ff6cceefd958b73ed6
peter.hortobagyi91@gmail.com <peter.hortobagyi91@gmail.com>
2023-05-06 19:22:21 +0200

    Now if filter lists are empty, all data returns.

--- Project/Services/CourseStatisticsService.cs
+++ Project/Services/CourseStatisticsService.cs
@@ -20,32 +20,70 @@ namespace Project.Services
             List<string> subjectNames,
             List<string> teacherNames)
         {
-            var result = await (from Course in _courseStatisticsContext.Courses
-                                join Subject in _courseStatisticsContext.Subjects on Course.SubjectId equals Subject.Id
-                                join TeacherOnCourse in _courseStatisticsContext.TeachersOnCourses on Course.Id equals TeacherOnCourse.CourseId
-                                join StudentOnCourse in _courseStatisticsContext.StudentsOnCourses on Course.Id equals StudentOnCourse.CourseId
-                                join Teacher in _courseStatisticsContext.Teachers on TeacherOnCourse.TeacherId equals Teacher.Id
-                                where semesterNames.Contains(Course.Semester) && subjectCodes.Contains(Subject.Code) && subjectNames.Contains(Subject.Name) && teacherNames.Contains(Teacher.Name)
-                                group new { Teacher, TeacherOnCourse, Subject, Course, StudentOnCourse } by new { TeacherId = Teacher.Id, SubjectId = Subject.Id, CourseId = Course.Id } into grp
-                                select new CourseStatisticsDto
-                                {
-                                    TeacherName = grp.First().Teacher.Name,
-                                    TeacherProportion = grp.First().TeacherOnCourse.Proportion,
-                                    CourseCode = grp.First().Course.Code,
-                                    CourseSemester = grp.First().Course.Semester,
-                                    CourseType = grp.First().Course.Type,
-                                    CourseProgram = grp.First().Course.Program,
-                                    CourseLanguage = grp.First().Course.Language,
-                                    SubjectName = grp.First().Subject.Name,
-                                    SubjectCode = grp.First().Subject.Code,
-                                    SignaturePerEnrollment = 1.0 * grp.Count(s => s.StudentOnCourse.DateOfSignature != null) / grp.Count(),
-                                    CompletedPerSignature = grp.Count(s => s.StudentOnCourse.DateOfSignature != null) == 0 ? null : 1.0 * grp.Count(s => s.StudentOnCourse.Completed == true) / grp.Count(s => s.StudentOnCourse.DateOfSignature != null),
-                                    CompletedPerEnrollment = 1.0 * grp.Count(s => s.StudentOnCourse.Completed == true) / grp.Count(),
-                                    NumberOfEnrollment = grp.Count(),
-                                    NumberOfSignature = grp.Count(s => s.StudentOnCourse.DateOfSignature != null),
-                                    NumberOfSignatureRefusal = grp.Count(s => s.StudentOnCourse.DateOfSignatureRefusal != null),
-                                    NumberOfCompleted = grp.Count(s => s.StudentOnCourse.Completed == true)
-                                }).ToListAsync();
+            IQueryable<CourseStatisticsDto> query;
+
+            if (semesterNames.IsNullOrEmpty()
+                && subjectCodes.IsNullOrEmpty()
+                && subjectNames.IsNullOrEmpty()
+                && teacherNames.IsNullOrEmpty())
+            {
+                query = from Course in _courseStatisticsContext.Courses
+                        join Subject in _courseStatisticsContext.Subjects on Course.SubjectId equals Subject.Id
+                        join TeacherOnCourse in _courseStatisticsContext.TeachersOnCourses on Course.Id equals TeacherOnCourse.CourseId
+                        join StudentOnCourse in _courseStatisticsContext.StudentsOnCourses on Course.Id equals StudentOnCourse.CourseId
+                        join Teacher in _courseStatisticsContext.Teachers on TeacherOnCourse.TeacherId equals Teacher.Id
+                        group new { Teacher, TeacherOnCourse, Subject, Course, StudentOnCourse } by new { TeacherId = Teacher.Id, SubjectId = Subject.Id, CourseId = Course.Id } into grp
+                        select new CourseStatisticsDto
+                        {
+                            TeacherName = grp.First().Teacher.Name,
+                            TeacherProportion = grp.First().TeacherOnCourse.Proportion,
+                            CourseCode = grp.First().Course.Code,
+                            CourseSemester = grp.First().Course.Semester,
+                            CourseType = grp.First().Course.Type,
+                            CourseProgram = grp.First().Course.Program,
+                            CourseLanguage = grp.First().Course.Language,
+                            SubjectName = grp.First().Subject.Name,
+                            SubjectCode = grp.First().Subject.Code,
+                            SignaturePerEnrollment = 1.0 * grp.Count(s => s.StudentOnCourse.DateOfSignature != null) / grp.Count(),
+                            CompletedPerSignature = grp.Count(s => s.StudentOnCourse.DateOfSignature != null) == 0 ? null : 1.0 * grp.Count(s => s.StudentOnCourse.Completed == true) / grp.Count(s => s.StudentOnCourse.DateOfSignature != null),
+                            CompletedPerEnrollment = 1.0 * grp.Count(s => s.StudentOnCourse.Completed == true) / grp.Count(),
+                            NumberOfEnrollment = grp.Count(),
+                            NumberOfSignature = grp.Count(s => s.StudentOnCourse.DateOfSignature != null),
+                            NumberOfSignatureRefusal = grp.Count(s => s.StudentOnCourse.DateOfSignatureRefusal != null),
+                            NumberOfCompleted = grp.Count(s => s.StudentOnCourse.Completed == true)
+                        };
+            }
+            else
+            {
+                query = from Course in _courseStatisticsContext.Courses
+                        join Subject in _courseStatisticsContext.Subjects on Course.SubjectId equals Subject.Id
+                        join TeacherOnCourse in _courseStatisticsContext.TeachersOnCourses on Course.Id equals TeacherOnCourse.CourseId
+                        join StudentOnCourse in _courseStatisticsContext.StudentsOnCourses on Course.Id equals StudentOnCourse.CourseId
+                        join Teacher in _courseStatisticsContext.Teachers on TeacherOnCourse.TeacherId equals Teacher.Id
+                        where semesterNames.Contains(Course.Semester) && subjectCodes.Contains(Subject.Code) && subjectNames.Contains(Subject.Name) && teacherNames.Contains(Teacher.Name)
+                        group new { Teacher, TeacherOnCourse, Subject, Course, StudentOnCourse } by new { TeacherId = Teacher.Id, SubjectId = Subject.Id, CourseId = Course.Id } into grp
+                        select new CourseStatisticsDto
+                        {
+                            TeacherName = grp.First().Teacher.Name,
+                            TeacherProportion = grp.First().TeacherOnCourse.Proportion,
+                            CourseCode = grp.First().Course.Code,
+                            CourseSemester = grp.First().Course.Semester,
+                            CourseType = grp.First().Course.Type,
+                            CourseProgram = grp.First().Course.Program,
+                            CourseLanguage = grp.First().Course.Language,
+                            SubjectName = grp.First().Subject.Name,
+                            SubjectCode = grp.First().Subject.Code,
+                            SignaturePerEnrollment = 1.0 * grp.Count(s => s.StudentOnCourse.DateOfSignature != null) / grp.Count(),
+                            CompletedPerSignature = grp.Count(s => s.StudentOnCourse.DateOfSignature != null) == 0 ? null : 1.0 * grp.Count(s => s.StudentOnCourse.Completed == true) / grp.Count(s => s.StudentOnCourse.DateOfSignature != null),
+                            CompletedPerEnrollment = 1.0 * grp.Count(s => s.StudentOnCourse.Completed == true) / grp.Count(),
+                            NumberOfEnrollment = grp.Count(),
+                            NumberOfSignature = grp.Count(s => s.StudentOnCourse.DateOfSignature != null),
+                            NumberOfSignatureRefusal = grp.Count(s => s.StudentOnCourse.DateOfSignatureRefusal != null),
+                            NumberOfCompleted = grp.Count(s => s.StudentOnCourse.Completed == true)
+                        };
+            }
+
+            var result = await query.ToListAsync();
 
             return result;
         }
--- Project/Services/SurvivalAnalysisService.cs
+++ Project/Services/SurvivalAnalysisService.cs
@@ -15,7 +15,11 @@ namespace Project.Services
 
         public async Task<IEnumerable<SurvivalAnalysisDto>> GetSurvivalAnalysisStatistics(List<string?> neptunCodes)
         {
-            var query = from s in _survivalAnalysisContext.SurvivalAnalysisItems
+            IQueryable<SurvivalAnalysisDto> query;
+
+            if (neptunCodes.IsNullOrEmpty())
+            {
+                query = from s in _survivalAnalysisContext.SurvivalAnalysisItems
                         where (s.EntryValue == "Elégséges" ||
                                s.EntryValue == "Közepes" ||
                                s.EntryValue == "Jó" ||
@@ -23,7 +27,6 @@ namespace Project.Services
                                s.EntryValue == "Jeles" ||
                                s.EntryValue == "Kiválóan megfelelt (5)")
                               && s.EnrollmentCredit != 0
-                              && neptunCodes.Contains(s.NeptunCode)
                         group s by new { s.NeptunCode, s.AdmissionSemester, s.Semester, s.SubjectCode } into g
                         select new
                         {
@@ -31,7 +34,7 @@ namespace Project.Services
                             g.Key.AdmissionSemester,
                             g.Key.Semester,
                             Credit = g.Max(x => x.EnrollmentCredit),
-                            Grade = g.Max(x => 
+                            Grade = g.Max(x =>
                             x.EntryValue == "Elégséges" ? 2 :
                             x.EntryValue == "Közepes" ? 3 :
                             x.EntryValue == "Jó" ? 4 :
@@ -47,6 +50,42 @@ namespace Project.Services
                             Credit = g.Sum(x => x.Credit),
                             WeightedGradePointAverage = g.Sum(x => x.Credit * 1.0 * x.Grade) / g.Sum(x => x.Credit)
                         };
+            }
+            else
+            {
+                query = from s in _survivalAnalysisContext.SurvivalAnalysisItems
+                        where (s.EntryValue == "Elégséges" ||
+                               s.EntryValue == "Közepes" ||
+                               s.EntryValue == "Jó" ||
+                               s.EntryValue == "Jól megfelelt" ||
+                               s.EntryValue == "Jeles" ||
+                               s.EntryValue == "Kiválóan megfelelt (5)")
+                              && s.EnrollmentCredit != 0
+                              && neptunCodes.Contains(s.NeptunCode)
+                        group s by new { s.NeptunCode, s.AdmissionSemester, s.Semester, s.SubjectCode } into g
+                        select new
+                        {
+                            g.Key.NeptunCode,
+                            g.Key.AdmissionSemester,
+                            g.Key.Semester,
+                            Credit = g.Max(x => x.EnrollmentCredit),
+                            Grade = g.Max(x =>
+                            x.EntryValue == "Elégséges" ? 2 :
+                            x.EntryValue == "Közepes" ? 3 :
+                            x.EntryValue == "Jó" ? 4 :
+                            x.EntryValue == "Jól megfelelt" ? 4 : 5)
+                        } into tbl
+                        group tbl by new { tbl.NeptunCode, tbl.AdmissionSemester } into g
+                        orderby g.Key.NeptunCode
+                        select new SurvivalAnalysisDto
+                        {
+                            NeptunCode = g.Key.NeptunCode,
+                            AdmissionSemester = g.Key.AdmissionSemester,
+                            SemesterCount = g.Select(x => x.Semester).Distinct().Count(),
+                            Credit = g.Sum(x => x.Credit),
+                            WeightedGradePointAverage = g.Sum(x => x.Credit * 1.0 * x.Grade) / g.Sum(x => x.Credit)
+                        };
+            }
 
             var result = await query.ToListAsync();
 
